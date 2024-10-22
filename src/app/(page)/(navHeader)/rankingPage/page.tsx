909804ceb7f4d495f4e35a5eb1cd83b13ac944bf
NautilusCode <xzhouzhi@gmail.com>
2024-10-22 09:17:22 +0900

    ランキングページの作り直し

--- src/app/(page)/(navHeader)/rankingPage/page.tsx
+++ src/app/(page)/(navHeader)/rankingPage/page.tsx
@@ -1,106 +1,45 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import {
-  collection,
-  getDocs,
-  query,
-  orderBy,
-  where,
-  limit,
-} from "firebase/firestore";
-import { db, auth } from "@/lib/firebase/client";
-import { onAuthStateChanged } from "firebase/auth";
-import Header from "@/app/layout/header/header";
-import Footer from "@/app/layout/footer/footer";
-import Loading from "@/app/components/loading";
+import { auth } from "@/lib/auth";
+import { prisma } from "../../../../../prisma/client";
 
 type GameResult = {
+  highScore: number | null;
   userId: string;
   userName: string;
   score: number;
 };
 
-const Ranking = () => {
-  const [rankings, setRankings] = useState<GameResult[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [currentUserId, setCurrentUserId] = useState<string | null>(null);
-  const [highScore, setHighScore] = useState<number | null>(null);
+const Ranking = async () => {
+  const session = await auth();
+  if (!session) return null;
 
-  useEffect(() => {
-    const fetchCurrentUser = () => {
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        if (user) {
-          setCurrentUserId(user.uid);
-          fetchUserHighScore(user.uid); // ユーザーのハイスコアを取得
-        } else {
-          setCurrentUserId(null);
+  const fetchScore = await prisma.gameResult.aggregate({
+    where: {
+      userid: session.user?.id
+    },
+    _max: {
+      score: true,
+    }
+  })
+
+  if (!fetchScore) return null;
+  const highScore = fetchScore._max.score
+
+  const rankings = await prisma.gameResult.findMany({
+    orderBy: {
+      score: 'desc',
+    },
+    take: 20,
+    include: {
+      user: {
+        select: {
+          name: true
         }
-      });
-      return () => unsubscribe();
-    };
-
-    const fetchUserHighScore = async (userId: string) => {
-      try {
-        const q = query(
-          collection(db, "gameResults"),
-          where("userId", "==", userId),
-          orderBy("score", "desc")
-        );
-        const querySnapshot = await getDocs(q);
-
-        if (!querySnapshot.empty) {
-          const highestScore = querySnapshot.docs[0].data().score;
-          setHighScore(highestScore);
-        }
-      } catch (error) {
-        console.error("Error fetching user high score: ", error);
       }
-    };
-
-    const fetchRankings = async () => {
-      try {
-        const q = query(
-          collection(db, "gameResults"),
-          orderBy("score", "desc"),
-          limit(10)
-        );
-        const querySnapshot = await getDocs(q);
-        const fetchedRankings: GameResult[] = [];
-
-        querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          fetchedRankings.push({
-            userId: data.userId,
-            userName: data.userName,
-            score: data.score,
-          });
-        });
-
-        setRankings(fetchedRankings);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching rankings: ", error);
-        setLoading(false);
-      }
-    };
-
-    fetchCurrentUser();
-    fetchRankings();
-  }, []);
-
-  // if (loading) {
-  //   return <div className="fixed inset-0 bg-gray-900 text-white text-2xl"> 
-  //           <div className="flex items-center justify-center h-screen">
-  //             <img className="mr-4" src="/image/stack-of-books.png" alt="" />
-  //             <p>Loading...</p>
-  //           </div>
-  //         </div>;
-  // }
+    }
+  })
 
   return (
     <div>
-      <Loading />
       <div
         className="bg-[url('../../public/image/bg-ranking.webp')] bg-cover bg-[rgba(0,0,0,0.60)] bg-blend-overlay bg-fixed"
       >
@@ -128,11 +67,11 @@ const Ranking = () => {
                     <tr
                       key={index}
                       className={
-                        ranking.userId === currentUserId ? "text-yellow-300" : "text-white"
+                        ranking.userid === session.user?.id ? "text-yellow-300" : "text-white"
                       }
                     >
                       <td className="text-center px-4 py-2">{index + 1}</td>
-                      <td className="text-center px-4 py-2">{ranking.userName}</td>
+                      <td className="text-center px-4 py-2">{ranking.user.name}</td>
                       <td className="text-center px-4 py-2">{ranking.score}</td>
                     </tr>
                   ))}
